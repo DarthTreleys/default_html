e13f7a45b640328ddf5c9ce61b7ac9f9c73b57bf
Treleys <joancosdo@gmail.com>
2025-04-10 19:14:47 +0200

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -3,7 +3,7 @@
 <head><title>Formulario</title></head>
 <body>
     <h2>Introduce tus datos</h2>
-    <form action="procesar.php" method="POST">
+    <form action="procesar.php" method="GET">
         Nombre: <input type="text" name="nombre"><br>
         Edad: <input type="number" name="edad"><br>
         <input type="submit" value="Enviar">
