81daa2ef62b60c8642dc32276178db6b49b43cfb
Treleys <joancosdo@gmail.com>
2025-04-10 17:09:59 +0200

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head><title>Formulario</title></head>
+<body>
+    <h2>Introduce tus datos</h2>
+    <form action="procesar.php" method="POST">
+        Nombre: <input type="text" name="nombre"><br>
+        Edad: <input type="number" name="edad"><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
